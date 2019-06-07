c47be0a99c486be94a34aa9ce00329e198b17679
Renato Reis <email@renatoamreis.com>
2019-06-07 15:59:10 +0200

    Adding the final Front-end Project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,123 @@
-import React from 'react';
-import Navigation from './components/navigation/Navigation'
-import Logo from './components/logo/Logo'
-import './App.css';
+import React, { Component } from "react";
+import Particles from "react-particles-js";
+import Clarifai from "clarifai";
+import Navigation from "./components/Navigation/Navigation";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      {/* <ImageLinkForm />
-      <FaceRecognition /> */}
-    </div>
-  );
+const app = new Clarifai.App({
+  apiKey: "0c081cda46a448468c6c184c146747c5"
+});
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: "",
+      box: {},
+      route: 'signin',
+      isSignedIn: false
+    };
+  }
+
+  //calculateFaceLocation() is to get the data about face location
+  //And we do the calculations to get the location for the face <div>
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('inputimage')
+    const width = Number(image.width)
+    const height = Number(image.height)
+    const person = data.outputs[0].data.regions[0].data.concepts[0].name
+    const probability = data.outputs[0].data.regions[0].data.concepts[0].value
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height),
+      person: person,
+      probability: `${Math.floor(probability * 100)} %` 
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box})
+  }
+
+  onInputChange = event => {
+    this.setState({input: event.target.value});
+  };
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input})
+    app.models
+      .predict(
+        "e466caa0619f444ab97497640cefc4dc",
+        this.state.input
+      )
+      //calculateFaceLocation() is to get the data about face location
+      //displayFaceBox() is to pass the returned values to state.box 
+      //The values from state.box are useful to create the div in "FaceRecgnition.js"
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
+  };
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route})
+  }
+
+  render() {
+    const { isSignedIn, imageUrl, route, box } = this.state
+    return (
+      <div className="App">
+        <Particles
+          className="particles"
+          params={{
+            particles: {
+              number: {
+                value: 60,
+                density: {
+                  enable: true,
+                  value_area: 800
+                }
+              }
+            }
+          }}
+        />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+
+        {/* The default route is signin, in state above, however if we click on signin 
+        it will route us to 'home' and render the other Components after -> : <- */}
+        { route === 'home' 
+          ? <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm
+                onInputChange={this.onInputChange}
+                onButtonSubmit={this.onButtonSubmit}
+              />
+              <FaceRecognition box={box} imageUrl={imageUrl}/>
+            </div>  
+          : (
+            route === 'signin' 
+            ? <Signin onRouteChange={this.onRouteChange}/>
+            : <Register onRouteChange={this.onRouteChange}/>
+          )
+
+      }
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Tilt from "react-tilt";
+import brain from './brain.png'
 import './Logo.css';
 
 const Logo = () => {
   return (
     <div className="ma4 mt0">
-        <Tilt className="Tilt" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
-            <div className="Tilt-inner"> 👽 </div>
+        <Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
+            <div className="Tilt-inner"> <img alt='logo' src={brain} /> </div>
         </Tilt>
     </div>
   );
