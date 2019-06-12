40cab4e91d3c1778f6244a6ab55560c0d8a8a1cb
Renato Reis <email@renatoamreis.com>
2019-06-12 17:11:39 +0200

    Updates

---
+++ precache-manifest.b2a9341a1e8c311ccb34c78891e5db0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1892e46512975bbb3d5873ce1fe8c49",
+    "url": "/face-recognition-brain/index.html"
+  },
+  {
+    "revision": "ac393f75b4da80457726",
+    "url": "/face-recognition-brain/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "96bdf2aa8b0e440adcb1",
+    "url": "/face-recognition-brain/static/css/main.e2fc9dfa.chunk.css"
+  },
+  {
+    "revision": "ac393f75b4da80457726",
+    "url": "/face-recognition-brain/static/js/2.33497142.chunk.js"
+  },
+  {
+    "revision": "96bdf2aa8b0e440adcb1",
+    "url": "/face-recognition-brain/static/js/main.880a84e2.chunk.js"
+  },
+  {
+    "revision": "6e37b30b4e95597c3d6b",
+    "url": "/face-recognition-brain/static/js/runtime~main.6459330c.js"
+  },
+  {
+    "revision": "8b80ab2ad2f0a933dbcf1e9257aea477",
+    "url": "/face-recognition-brain/static/media/brain.8b80ab2a.png"
+  }
+]);
